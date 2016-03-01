f62580e5f6bbf4867c745caa9164b019f16d5630
Setiaji Kurniawan <setiaji.kurniawan@gmail.com>
2016-03-01 19:34:46 +0700

    Update session

--- app/views.py
+++ app/views.py
@@ -1,23 +1,35 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
+# index view function suppresed for brevity
 
-def index():
-    user = {'nickname' : 'Setiaji'} #fake user
-    posts = [ # fake array of posts
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-     ]
-    return render_template('index.html',
-                           title='',
-                           user=user,
-                           posts=posts)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(csrf_enabled=False)
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form)
+#@app.route('/index')
+
+#def index():
+#    user = {'nickname' : 'Setiaji'} #fake user
+#    posts = [ # fake array of posts
+#        {
+#            'author': {'nickname': 'John'},
+#            'body': 'Beautiful day in Portland!'
+#        },
+#        {
+#            'author': {'nickname': 'Susan'},
+#            'body': 'The Avengers movie was so cool!'
+#        }
+#     ]
+#    return render_template('index.html',
+#                           title='',
+#                           user=user,
+#                           posts=posts)
     # return "Hello world"
