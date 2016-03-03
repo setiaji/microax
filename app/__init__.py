7c32c0f05c54099f7fbcfebafbcc0935dab7cf28
Setiaji Kurniawan <setiaji.kurniawan@gmail.com>
2016-03-03 15:41:29 +0700

    Adding SECRET_KEY

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+
 from app import views
+
+app.secret_key = 'you-will-never-guess'
+
--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from .forms import LoginForm
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm(csrf_enabled=False)
+    form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for OpenID="%s", remember_me=%s' %
               (form.openid.data, str(form.remember_me.data)))
